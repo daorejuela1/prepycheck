c5cb697ad1138aca5bf294ba1fe398e2d07fbc41
daorejuela1 <vids@hotmail.es>
2020-06-24 05:16:31 +0000

    check that user is sudo when installing script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-sudo mkdir -p /usr/local/scripts
-cp prepycheck.sh /usr/local/scripts
+
+if [ "$(id -u)" != "0" ]
+then
+	echo "Sorry, you are not root."
+	exit 1
+fi
+
+sudo mkdir -p /usr/local/scripts/
+cp prepycheck.sh /usr/local/scripts/
 chmod +x /usr/local/scripts/prepycheck.sh
 echo "alias prepycheck='/usr/local/scripts/./prepycheck.sh'" >> ~/.bash_aliases
 source ~/.bash_aliases
